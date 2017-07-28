0d9cb6d71d2ba5be3aa8ddfcd2b0795646832a1d
root <root@localhost.localdomain>
2017-07-28 13:42:09 +0900

    coreen footer

---
+++ templates/includes/footer-coreen.php
@@ -0,0 +1,24 @@
+<?php
+if(!empty($this->data['htmlinject']['htmlContentPost'])) {
+	foreach($this->data['htmlinject']['htmlContentPost'] AS $c) {
+		echo $c;
+	}
+}
+?>
+<!-- layout-footer -->
+<div id="layout-footer">
+	<div id="footer">
+		<p class="kafe">
+			<img src="/<?php echo $this->data['baseurlpath']; ?>resources/coreen/images/kafe.png" alt="KAFE" />
+			Korean Access FEderation
+		</p>
+
+		<ul id="fnb">
+			<li><a href="https://coreen.kreonet.net/privacy_policy" target="_blank">PRIVACY POLICY</a></li>
+			<li><a href="https://coreen.kreonet.net/user_agreement" target="_blank">TERMS OF USE</a></li>
+		</ul>
+	</div>
+</div>
+<!-- //layout-footer -->
+</body>
+</html>
